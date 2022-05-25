92ba5cadf86b43c9ab08adf3988008d89fc78abf
Pawfa <fajfer.pawel@gmail.com>
2022-05-25 17:40:45 +0200

    fix: Do not include files with "package" substring and without full stop

--- src/core/isFromTheSamePackage.ts
+++ src/core/isFromTheSamePackage.ts
@@ -1,16 +1,17 @@
 import path from "path";
 import { makePathPosix } from "../utils/path.utils";
+import { hasPackageInName } from "./hasPackageInName";
 
 export function isFromTheSamePackage(importedFileName: string, currentFileName: string, packageNames: string[]) {
   const importFileNameSplitted = makePathPosix(path.normalize(importedFileName)).split(path.posix.sep);
   const currentFileNameSplitted = makePathPosix(path.normalize(currentFileName)).split(path.posix.sep);
 
   const importedPackageName = importFileNameSplitted
-    .find((el) => el.includes("package") || packageNames.includes(el))
+    .find((el) => hasPackageInName(el) || packageNames.includes(el))
     ?.replace(/package|\./g, "");
 
   const currentPackageName = currentFileNameSplitted
-    .find((el) => el.includes("package") || packageNames.includes(el))
+    .find((el) => hasPackageInName(el) || packageNames.includes(el))
     ?.replace(/package|\./g, "");
 
   return importedPackageName && currentPackageName && importedPackageName === currentPackageName;
