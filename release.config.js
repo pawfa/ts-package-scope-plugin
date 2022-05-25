6224f08668fd03b95589146d1e3e75bd8deff405
Pawfa <fajfer.pawel@gmail.com>
2022-05-25 19:27:55 +0200

    build: Prevent semantic-release from creating issues

--- release.config.js
+++ release.config.js
@@ -3,7 +3,13 @@ module.exports = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/npm",
-    "@semantic-release/github",
+    [
+      "@semantic-release/github",
+      {
+        failComment: false,
+        successComment: false,
+      },
+    ],
   ],
   branches: ["main"],
 };
