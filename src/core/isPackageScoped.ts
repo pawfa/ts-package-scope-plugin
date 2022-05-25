15e09c5705eb159e5d741dd33e86ac7d9301493d
Pawfa <fajfer.pawel@gmail.com>
2022-05-25 17:40:45 +0200

    fix: Do not include files with "package" substring and without full stop

--- src/core/isPackageScoped.ts
+++ src/core/isPackageScoped.ts
@@ -1,11 +1,12 @@
 import path from "path";
 import { makePathPosix } from "../utils/path.utils";
+import { hasPackageInName } from "./hasPackageInName";
 
 export function isPackageScoped(fileName: string, packageNames: string[]) {
   const fileNameSplitted = makePathPosix(path.normalize(fileName)).split(path.posix.sep);
 
   return (
-    fileNameSplitted.some((name) => name.includes("package")) ||
+    fileNameSplitted.some((name) => hasPackageInName(name)) ||
     packageNames.some((packageName) => fileNameSplitted.includes(packageName))
   );
 }
